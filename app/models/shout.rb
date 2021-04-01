dfe990732c70b6efa7f512c03f5972ad257c4849
Maxi Amodei <maxi_amodei@hotmail.com>
2021-04-01 12:56:54 -0300

    Creating show views for users and messages

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -1,5 +1,14 @@
 class Shout < ApplicationRecord
   belongs_to :user
+
   validates :body, presence: true, length: {in: 1..144}
   validates :user, presence: true
+
+  #Asi ordeno el listado de mensages en otro orden
+  default_scope { order(created_at: :desc) }
+
+  delegate :username, to: :user
+  # def username
+  #   self.user.username
+  # end
 end
