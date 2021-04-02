86dff1bddf2173baf25d47b66310f9710a204273
Maxi Amodei <maxi_amodei@hotmail.com>
2021-04-01 21:35:06 -0300

    Fixing bug with photo attachment

--- app/models/photo_shout.rb
+++ app/models/photo_shout.rb
@@ -1,8 +1,10 @@
 class PhotoShout < ApplicationRecord
   has_attached_file :image, style: { thumb: "200x200" }
+    # :default_url => "path to default image"
+
 
   validates_attachment :image,
-    content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] },
+    content_type: { content_type: ["image/jpeg", "image/gif", "image/png", "image/jpg"] },
     size: { in: 0..10.megabytes },
     presence: true
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,8 @@ Rails.application.configure do
   # Clearance config
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   # Settings specified here will take precedence over those in config/application.rb.
-
+  # Paperclip config
+  Paperclip.options[:command_path] = "/usr/bin/"
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
