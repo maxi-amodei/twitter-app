e7869009f3e16603a1900a5ec4fe5ee3ae5f8163
Maxi Amodei <maxi_amodei@hotmail.com>
2021-04-02 10:37:22 -0300

    Adding restrictions for unregistered users to like

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
+  
+  def current_user
+    # This is to handle unregistered users entering the users page
+    super || Guest.new
+  end
 end
